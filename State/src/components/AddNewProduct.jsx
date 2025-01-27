4b69f0c6e315cffb3ddb4fc369498e555b2dde45
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-01-28 00:14:48 +0300

    Arrow Function

--- State/src/components/AddNewProduct.jsx
+++ State/src/components/AddNewProduct.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import { useAddNewProductMutation } from '../app/service/dummyData'
 
 const AddNewProduct = () => {
+
+    const [addNewProduct, { data, error, isLoading }] = useAddNewProductMutation();
+    // console.log(res);
+
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>Error</h1>
+    }
+
+    const handleAddProduct = async () => {
+
+        try {
+
+            const newProductData = {
+                id: 1,
+                title: "Amazing T-Shirt",
+                description: "This is an amazing T-Shirt",
+            }
+
+        } catch (err) {
+            console.log("Error adding new product", err);
+        }
+
+    }
     return (
         <div>
-            AddNewProduct
+            <button onClick={handleAddProduct} disabled={isLoading}>Add New Product</button>
         </div>
     )
 }
