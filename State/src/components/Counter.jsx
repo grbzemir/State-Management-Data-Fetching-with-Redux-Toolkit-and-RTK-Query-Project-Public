cc91f7a90461d31ef3d51d06a7b97e10cce7f184
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-01-27 23:01:57 +0300

    Counter

---
+++ State/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import React from 'react'
+import { increment, decrement, incrementByAmount } from '../app/features/counter/counterSlice';
+
+const Counter = () => {
+
+    //Read the data from the "store"
+    const count = useSelector(state => state.counter.value);
+
+    //Dispatch the action to the "store"
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+
+            <h1>{count}</h1>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(incrementByAmount())}></button>
+
+        </div>
+    )
+}
+
+export default Counter
