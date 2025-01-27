068848010aca11832cc1bec675fe35a6f57cd9be
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-01-27 23:29:47 +0300

    dummyData

--- State/src/app/service/dummyData.jsx
+++ State/src/app/service/dummyData.jsx
@@ -1,13 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-export const dummyApi = createApi({
+export const productsApi = createApi({
     reducerPath: "products",
     baseQuery: fetchBaseQuery({ baseUrl: "https://dummyjson.com" }),
     endpoints: (builder) => ({
         getAllProducts: builder.query({
-            query: () => "/api/products",
+            query: () => "/products",
         }),
     }),
-
 });
+
+// `use${GetAllProduct}Query`
+
+export const { useGetAllProductQuery } = productsApi;
