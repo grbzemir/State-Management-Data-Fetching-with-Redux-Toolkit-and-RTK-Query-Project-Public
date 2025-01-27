827be1cf754339567d3b0b3c3361a64bb39bf123
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-01-27 23:34:36 +0300

    Fetch

--- State/src/app/store.jsx
+++ State/src/app/store.jsx
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { productsApi } from './service/dummyData';
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
+        [productsApi.reducerPath]: productsApi.reducer,
     },
+
+
 });
